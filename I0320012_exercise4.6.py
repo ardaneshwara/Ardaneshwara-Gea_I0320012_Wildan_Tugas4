50b45115b8a5f7bd17a40f893f56e432b0e15c56
ardaneshwara <ardaneshwaragea2@gmail.com>
2021-03-22 21:30:18 +0700

    Pengerjaan Exercise Week 4

---
+++ I0320012_exercise4.6.py
@@ -0,0 +1,13 @@
+# strings
+
+needle = "HA"
+
+haystack = "Hello World"
+
+# check
+
+if needle in haystack:
+    print(needle, "is present in the strings", haystack)
+
+else:
+    print("Not found")
---
+++ I0320012_exercise4.9.py
@@ -0,0 +1,11 @@
+# string
+
+str = "Hello World"
+
+# substring
+
+substr= str[3:5]
+
+# output
+
+print(substr)   #lo
