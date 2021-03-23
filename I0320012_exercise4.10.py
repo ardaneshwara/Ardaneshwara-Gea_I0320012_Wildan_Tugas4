03f876ce567f9485cd3fb572b69de332efe3c8af
ardaneshwara <ardaneshwaragea2@gmail.com>
2021-03-23 19:18:00 +0700

    Pengerjaan Exercise Week 4

--- I0320012_exercise4.10.py
+++ I0320012_exercise4.10.py
@@ -1,9 +1,3 @@
-string: Hello World
-
-skipping:   x x x x x     # characters marked with x
-
-final str: HloWrd
-
 # string
 
 str = "Hello World"
@@ -11,3 +5,5 @@ str = "Hello World"
 # skip
 
 new_str = str[0::2]
+
+print(new_str)
