c76757ec7f9d22381b464c6faf42dc9bcd27a80e
ardaneshwara <80144925+ardaneshwara@users.noreply.github.com>
2021-03-25 08:37:04 +0700

    Delete I0320012_soal3_tugas4.py

--- I0320012_soal3_tugas4.py
+++
@@ -1,5 +0,0 @@
-x = 50                                          # kode untuk berat maksimum
-y = int(input("Memasukkan berat bagasi : "))    # masukkan berat lebih 110 kg dan berat 49 kg
-
-print(y < x)
-
